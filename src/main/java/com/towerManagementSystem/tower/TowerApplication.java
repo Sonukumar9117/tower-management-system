7968764eb6a7f29fb782848d9d06cb78aff90742
Sonukumar9117 <krsonu1791@gmail.com>
2026-06-30 12:11:42 +0530

    feat:added-log

--- src/main/java/com/towerManagementSystem/tower/TowerApplication.java
+++ src/main/java/com/towerManagementSystem/tower/TowerApplication.java
@@ -1,13 +1,19 @@
 package com.towerManagementSystem.tower;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TowerApplication {
+public class TowerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TowerApplication.class, args);
+
+	 	SpringApplication.run(TowerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Your backend is running fine");
+	}
 }
